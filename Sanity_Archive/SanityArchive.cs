9d701c49f0b382b013075ae5770f165c09ae971e
csaszarhunor <csaszarhunor@gmail.com>
2016-04-19 17:15:55 +0200

    drives added

--- Sanity_Archive/SanityArchive.cs
+++ Sanity_Archive/SanityArchive.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,5 +24,13 @@ namespace Sanity_Archive
         {
 
         }
+
+        private void SanityArchive_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            drives_box.Items.AddRange(drives);
+
+        }
+        
     }
 }
