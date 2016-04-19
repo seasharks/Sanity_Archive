8142ec8a1075f0bb9a668e79c90f41903513b725
csaszarhunor <csaszarhunor@gmail.com>
2016-04-19 13:52:48 +0200

    size feature added

--- Sanity_Archive/Form1.Designer.cs
+++ Sanity_Archive/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.drive = new System.Windows.Forms.Label();
             this.drives_box = new System.Windows.Forms.ComboBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.fileFolder_box = new System.Windows.Forms.ListBox();
             this.encryption_bttn = new System.Windows.Forms.Button();
             this.compression_bttn = new System.Windows.Forms.Button();
             this.search_bttn = new System.Windows.Forms.Button();
             this.attributes_bttn = new System.Windows.Forms.Button();
+            this.size_lbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // drive
@@ -54,21 +55,21 @@
             this.drives_box.Size = new System.Drawing.Size(241, 21);
             this.drives_box.TabIndex = 1;
             // 
-            // listBox1
+            // fileFolder_box
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.fileFolder_box.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(16, 43);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(281, 147);
-            this.listBox1.TabIndex = 2;
+            this.fileFolder_box.FormattingEnabled = true;
+            this.fileFolder_box.Location = new System.Drawing.Point(16, 43);
+            this.fileFolder_box.Name = "fileFolder_box";
+            this.fileFolder_box.Size = new System.Drawing.Size(281, 173);
+            this.fileFolder_box.TabIndex = 2;
             // 
             // encryption_bttn
             // 
             this.encryption_bttn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.encryption_bttn.Location = new System.Drawing.Point(13, 197);
+            this.encryption_bttn.Location = new System.Drawing.Point(13, 244);
             this.encryption_bttn.Name = "encryption_bttn";
             this.encryption_bttn.Size = new System.Drawing.Size(139, 23);
             this.encryption_bttn.TabIndex = 3;
@@ -78,7 +79,7 @@
             // compression_bttn
             // 
             this.compression_bttn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.compression_bttn.Location = new System.Drawing.Point(158, 197);
+            this.compression_bttn.Location = new System.Drawing.Point(158, 244);
             this.compression_bttn.Name = "compression_bttn";
             this.compression_bttn.Size = new System.Drawing.Size(139, 23);
             this.compression_bttn.TabIndex = 4;
@@ -88,7 +89,7 @@
             // search_bttn
             // 
             this.search_bttn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.search_bttn.Location = new System.Drawing.Point(158, 226);
+            this.search_bttn.Location = new System.Drawing.Point(158, 273);
             this.search_bttn.Name = "search_bttn";
             this.search_bttn.Size = new System.Drawing.Size(139, 23);
             this.search_bttn.TabIndex = 6;
@@ -98,23 +99,33 @@
             // attributes_bttn
             // 
             this.attributes_bttn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.attributes_bttn.Location = new System.Drawing.Point(13, 226);
+            this.attributes_bttn.Location = new System.Drawing.Point(13, 273);
             this.attributes_bttn.Name = "attributes_bttn";
             this.attributes_bttn.Size = new System.Drawing.Size(139, 23);
             this.attributes_bttn.TabIndex = 5;
             this.attributes_bttn.Text = "Attributes";
             this.attributes_bttn.UseVisualStyleBackColor = true;
             // 
+            // size_lbl
+            // 
+            this.size_lbl.AutoSize = true;
+            this.size_lbl.Location = new System.Drawing.Point(16, 225);
+            this.size_lbl.Name = "size_lbl";
+            this.size_lbl.Size = new System.Drawing.Size(169, 13);
+            this.size_lbl.TabIndex = 7;
+            this.size_lbl.Text = "0.0 kB/0.0 kB 0/0 files 0/0 folders";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(314, 261);
+            this.ClientSize = new System.Drawing.Size(314, 308);
+            this.Controls.Add(this.size_lbl);
             this.Controls.Add(this.search_bttn);
             this.Controls.Add(this.attributes_bttn);
             this.Controls.Add(this.compression_bttn);
             this.Controls.Add(this.encryption_bttn);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.fileFolder_box);
             this.Controls.Add(this.drives_box);
             this.Controls.Add(this.drive);
             this.Name = "Form1";
@@ -128,11 +139,12 @@
 
         private System.Windows.Forms.Label drive;
         private System.Windows.Forms.ComboBox drives_box;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox fileFolder_box;
         private System.Windows.Forms.Button encryption_bttn;
         private System.Windows.Forms.Button compression_bttn;
         private System.Windows.Forms.Button search_bttn;
         private System.Windows.Forms.Button attributes_bttn;
+        private System.Windows.Forms.Label size_lbl;
     }
 }
 
