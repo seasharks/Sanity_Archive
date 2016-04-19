ca799243d16500c801d0817ec21476b7bbbb98a6
Slezak Attila <attila.slezak@gmail.com>
2016-04-19 16:27:14 +0100

    GUI ready, further changes expected

---
+++ Sanity_Archive/AttributesDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sanity_Archive
+{
+    public partial class AttributesDialog : Form
+    {
+        public AttributesDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
